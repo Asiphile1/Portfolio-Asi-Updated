a76ef35088eb4617b22f2532c697f93e17922b0e
asiphile <asiphilemthethethwa@gmail.com>
2025-01-27 08:01:06 +0200

    added final project text

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="TO-DO List App"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="This To-Do List Application is a full-featured web application designed for managing daily tasks with CRUD (Create, Read, Update, Delete) functionality. It allows users to register, log in, and manage a personalized to-do list with a user-friendly interface. The application is developed using HTML, CSS, JavaScript, SQLite, and Material UI for enhancing visual appeal. It is fully responsive and implements user authentication and data validation for secure usage."
               ghLink="	https://github.com/Asiphile1/To-do-list-app-codetribe"
               demoLink="https://to-do-list-app-codetribe.vercel.app/"
             />
@@ -37,7 +37,7 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="Employee Registration App"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              description="The Employee Management App is a simple and user-friendly React application designed to manage employee information. It allows users to add new employees, update existing employee details, and view employee information. The app uses local storage to persist data, ensuring that employee information is retained even after the page is refreshed."
               ghLink="https://github.com/Asiphile1/codeTribe-employee-app"
               demoLink="https://code-tribe-employee-app.vercel.app/"
             />
@@ -48,7 +48,7 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="Weather App"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="A React-based Weather Application that provides current weather conditions and hourly forecasts for any city. The app uses the OpenWeather API to fetch weather data and displays it with a user-friendly interface."
               ghLink="https://github.com/Asiphile1/Weather-App-CodeTribe"
               demoLink="https://weather-app-code-tribe.vercel.app/"
             />
@@ -59,7 +59,7 @@ function Projects() {
               imgPath={leaf}
               isBlog={false}
               title="Birthday Card"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              description="This is a React Native app that allows users to create custom birthday cards by adding images, text, and adjusting font sizes. The app includes features such as selecting an image from the gallery, editing text, and formatting the card for a personalized birthday greeting."
               ghLink="https://github.com/Asiphile1/Birthday-Card"
               demoLink="https://birthday-card-eta-virid.vercel.app/"
             />
@@ -70,7 +70,7 @@ function Projects() {
               imgPath={suicide}
               isBlog={false}
               title="Recipe App"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              description="The Online Recipe Application is a web-based platform that allows users to manage their favorite recipes. Built using ReactJS and JSON Server, this application provides users with the ability to store, search, add, delete, and update recipes. Users can register, log in, and securely manage their recipe collections. The app implements CRUD operations and ensures user-friendly navigation with responsive design."
               ghLink="https://github.com/Asiphile1/Recipe-App-CodeTribe"
               demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"  
             />
@@ -81,8 +81,7 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="Text effect animation"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              description="This project is a simple yet visually appealing text effect animation designed to enhance the user experience with creative and dynamic text transitions. The animation is customizable, lightweight, and easy to integrate into any web project."
               ghLink="https://github.com/Asiphile1/text-effect"
               demoLink="https://text-effect-od8u.vercel.app/"     
             />
